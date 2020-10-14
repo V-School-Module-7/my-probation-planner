d311a3e53e5f88ee1f3ac0d50d3ba00fe68ce1ee
Kela <kelabernesser@gmail.com>
2020-10-14 16:03:43 -0600

    fixing repo

---
+++ ReactNative/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LoginScreen from './app/screens/LoginScreen'
+import SignupScreen from './app/screens/SignupScreen'
+import DashboardScreen from './app/screens/DashboardScreen'
+import ForgotPasswordScreen from './app/screens/ForgotPasswordScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+
+const Stack = createStackNavigator()
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen}/>
+        <Stack.Screen name="SignupScreen" component={SignupScreen}/>
+        <Stack.Screen name="DashboardScreen" component={DashboardScreen}/>
+        <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
